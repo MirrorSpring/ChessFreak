123bc2c37df0a935616174a9d8ae7d61280df514
MirrorSpring <dbtmd619@naver.com>
2023-07-29 17:54:10 +0900

    배포를 위한 설정

--- ChessFreak/src/main/java/com/chessfreak/ChessFreakApplication.java
+++ ChessFreak/src/main/java/com/chessfreak/ChessFreakApplication.java
@@ -2,10 +2,16 @@ package com.chessfreak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChessFreakApplication {
+public class ChessFreakApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ChessFreakApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ChessFreakApplication.class, args);
 	}
